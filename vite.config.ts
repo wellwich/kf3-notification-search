6f7ae8b1500bd2430368e493d302fd3c7715a6ff
wellwich <mail@wellwich.com>
2024-10-06 19:32:37 +0900

    Refactor build configuration in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,14 @@
-import pages from "@hono/vite-cloudflare-pages";
-import adapter from "@hono/vite-dev-server/cloudflare";
 import honox from "honox/vite";
 import { defineConfig } from "vite";
-import client from "honox/vite/client";
-import build from "@hono/vite-build/bun";
+import build from "@hono/vite-build/cloudflare-pages";
 
-export default defineConfig(({ mode }) => {
-  if (mode === "client") {
-    return {
-      plugins: [client()],
-    };
-  } else {
-    return {
-      build: {
-        emptyOutDir: false,
+export default defineConfig({
+  plugins: [
+    honox({
+      client: {
+        input: ["/app/style.css"],
       },
-      plugins: [
-        honox({
-          client: { input: ["/app/style.css"] },
-          devServer: { adapter },
-        }),
-        pages(),
-        build(),
-      ],
-    };
-  }
+    }),
+    build(),
+  ],
 });
