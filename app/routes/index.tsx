a0cead832cd6b19a595735ea336a29ecc81115d3
reamkf <68076678+reamkf@users.noreply.github.com>
2024-11-24 13:32:38 +0900

    Adjust title padding

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -5,7 +5,7 @@ export default createRoute((c) => {
   const name = "けもフレ３おしらせ検索";
   return c.render(
     <div class="flex flex-col">
-      <h1 class="text-4xl font-bold p-4">{name}</h1>
+      <h1 class="text-4xl font-bold p-4 pb-0">{name}</h1>
       <KemonoFriends3 />
     </div>,
     { title: name }
